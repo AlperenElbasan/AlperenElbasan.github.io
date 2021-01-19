f6424c52dd98b7ee97fedbd675a18963b9683bca
Alperen <aalperenelbasan@gmail.com>
2021-01-18 23:43:52 -0600

    animations.js disable select type.

--- lab6/ascii.js
+++ lab6/ascii.js
@@ -1,32 +1,31 @@
+let timerId = null;
 
-async function startAnimation() {
-  const ANIMATIONS = (await import('./animations.js')).default;
-  switchButtonDisabled();
-
-  const whichOne = document.getElementById('animation-selector').value;
-  const animation = ANIMATIONS[whichOne];
+function startAnimation() {
+  switchButtonsDisabled();
+  const animationSelector = document.getElementById('animation-selector');
+  const animation = ANIMATIONS[animationSelector.value];
   document.getElementById('animation-area').value = animation;
   const moves = animation.split('=====\n');
   let index = 0;
-  document.getElementById('timer-id-container').value = setInterval(function () {
-    if (index >= moves.length - 1) {
+  timerId = setInterval(function () {
+    if (index > moves.length - 1)
       index = 0;
-    }
     document.getElementById('animation-area').value = moves[index++];
   }, 250);
 }
 
 async  function stopAnimation() {
-  clearInterval(document.getElementById('timer-id-container').value);
-  switchButtonDisabled();
-  const ANIMATIONS = (await import('./animations.js')).default;
-  const whichOne = 'JUGGLER';
-  document.getElementById('animation-area').value = ANIMATIONS[whichOne];
+  clearInterval(timerId);
+  switchButtonsDisabled();
+  const animationSelector = document.getElementById('animation-selector');
+  document.getElementById('animation-area').value = ANIMATIONS[animationSelector.value];
 }
 
-function switchButtonDisabled() {
-  const startButton = document.getElementById('start-button')
-  const stopButton = document.getElementById('stop-button')
+function switchButtonsDisabled() {
+  const startButton = document.getElementById('start-button');
+  const stopButton = document.getElementById('stop-button');
+  const animationSelector = document.getElementById('animation-selector');
   startButton.disabled = !startButton.disabled;
   stopButton.disabled = !stopButton.disabled;
+  animationSelector.disabled = !animationSelector.disabled;
 }
