d17375b5c71016d0e8f3dbb6baade153073d019d
Alperen <aalperenelbasan@gmail.com>
2021-01-18 23:15:28 -0600

    animations are working.

--- lab6/ascii.js
+++ lab6/ascii.js
@@ -1,18 +1,32 @@
 
-
 async function startAnimation() {
   const ANIMATIONS = (await import('./animations.js')).default;
-  const startButton = document.getElementById('start-button')
-  const stopButton = document.getElementById('stop-button')
-  startButton.disabled = true;
-  stopButton.disabled = false;
+  switchButtonDisabled();
+
+  const whichOne = document.getElementById('animation-selector').value;
+  const animation = ANIMATIONS[whichOne];
+  document.getElementById('animation-area').value = animation;
+  const moves = animation.split('=====\n');
+  let index = 0;
+  document.getElementById('timer-id-container').value = setInterval(function () {
+    if (index >= moves.length - 1) {
+      index = 0;
+    }
+    document.getElementById('animation-area').value = moves[index++];
+  }, 250);
+}
+
+async  function stopAnimation() {
+  clearInterval(document.getElementById('timer-id-container').value);
+  switchButtonDisabled();
+  const ANIMATIONS = (await import('./animations.js')).default;
   const whichOne = 'JUGGLER';
   document.getElementById('animation-area').value = ANIMATIONS[whichOne];
 }
 
-function stopAnimation() {
+function switchButtonDisabled() {
   const startButton = document.getElementById('start-button')
   const stopButton = document.getElementById('stop-button')
-  startButton.disabled = false;
-  stopButton.disabled = true;
+  startButton.disabled = !startButton.disabled;
+  stopButton.disabled = !stopButton.disabled;
 }
